80ef48933210f5de50e50f6b547e08815eddf7e1
James Powers <powersajames@gmail.com>
2018-03-30 12:57:04 -0700

    reworked for SSR

--- helpers/bundles.js
+++ helpers/bundles.js
@@ -1,31 +1,16 @@
-module.exports = 
+module.exports =
 [
-  { 
-    service: 'info',
-    url: `http://localhost:1127/`,
-    endpoint: 'about',
+  {
+    service: 'reviews',
+    url: `http://localhost:4004/`,
+    endpoint: 'reviews',
     fileName: {
       server: 'bundle-server.js',
       client: 'bundle.js',
     },
     props: {
-      id: 'restaurantId',
+      id: 'RestId',
       data: 'restaurant',
-    }
-  },
-  { 
-    service: 'suggested',
-    url: `http://localhost:3001/`,
-    endpoint: 'suggestions',
-    fileName: {
-      server: 'bundle-server.js',
-      client: 'bundle.js',
-      styles: 'styles.css'
-    },
-    props: {
-      id: 'id',
-      data: 'restaurants',
     }
   }
 ];
-
