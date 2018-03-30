5023108c563bc247db95ca6b6f45d2c506e6b2fa
jamesapowers <powersajames@gmail.com>
2018-03-30 13:10:13 -0700

    Update bundles.js

--- helpers/bundles.js
+++ helpers/bundles.js
@@ -1,16 +1,30 @@
 module.exports =
 [
   {
-    service: 'reviews',
-    url: `http://localhost:4004/`,
-    endpoint: 'reviews',
+    service: 'info',
+    url: `http://localhost:1127/`,
+    endpoint: 'about',
     fileName: {
       server: 'bundle-server.js',
       client: 'bundle.js',
     },
     props: {
-      id: 'RestId',
+      id: 'restaurantId',
       data: 'restaurant',
+    }
+  },
+  {
+    service: 'suggested',
+    url: `http://localhost:3001/`,
+    endpoint: 'suggestions',
+    fileName: {
+      server: 'bundle-server.js',
+      client: 'bundle.js',
+      styles: 'styles.css'
+    },
+    props: {
+      id: 'id',
+      data: 'restaurants',
     }
   }
 ];
